6186c7b67c1577c90a7be75c0a5c8c5694023b01
Fredy Mata <fredymata17@icloud.com>
2026-07-10 02:07:12 -0500

    Refactor and update notebooks: remove unused Bayesian logistic regression notebook, fix execution counts and errors in EDA notebooks, add MNIST CNN implementation, and introduce a new PyTorch neural network notebook with various layers and optimizers for future references.

--- notebooks/linear_algebra/image_detector.py
+++ notebooks/linear_algebra/image_detector.py
@@ -1,30 +1,47 @@
 import numpy as np
+from scipy.linalg import null_space
 
-dim = 9
-rand_vec = np.random.randint(0,2,dim)
+# dim = 9
+# rand_vec = np.random.randint(0, 2, dim)
 
-M = np.array([[1,0,0,0,0,0,0,0,0],
-              [0,1,0,0,0,0,0,0,0],
-              [0,0,4,-1,-1,-1,-1,0,0],
-              [0,0,-1,1,0,0,0,0,0],
-              [0,0,-1,0,1,0,0,0,0],
-              [0,0,-1,0,0,1,0,0,0],
-              [0,0,-1,0,0,0,1,0,0],
-              [0,0,0,0,0,0,0,1,0],
-              [0,0,0,0,0,0,0,0,1]])
+# M = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+#               [0, 1, 0, 0, 0, 0, 0, 0, 0],
+#               [0, 0, 4, -1, -1, -1, -1, 0, 0],
+#               [0, 0, -1, 1, 0, 0, 0, 0, 0],
+#               [0, 0, -1, 0, 1, 0, 0, 0, 0],
+#               [0, 0, -1, 0, 0, 1, 0, 0, 0],
+#               [0, 0, -1, 0, 0, 0, 1, 0, 0],
+#               [0, 0, 0, 0, 0, 0, 0, 1, 0],
+#               [0, 0, 0, 0, 0, 0, 0, 0, 1]])
 
-print(rand_vec.T @ M @ rand_vec)
+# print(rand_vec.T @ M @ rand_vec)
 
-my_vec = np.array([0,0,1,1,1,1,1,0,0])
+# my_vec = np.array([0, 0, 1, 1, 1, 1, 1, 0, 0])
 
-print(my_vec.T @ M @ my_vec)
+# print(my_vec.T @ M @ my_vec)
 
 
-###############################
-for _ in range(10000):
-    rand_vec = np.random.randint(0,2,dim)
-    
-    res = rand_vec.T @ M @ rand_vec
-    
-    if res == 0:
-        print(rand_vec)
+# ###############################
+# for _ in range(10000):
+#     rand_vec = np.random.randint(0, 2, dim)
+
+#     res = rand_vec.T @ M @ rand_vec
+
+#     if res == 0:
+#         print(rand_vec)
+
+
+def create_image():
+    """Guide the user through selecting their vector."""
+    D: int = int(input("How many bits will your image have?\n"))
+    bits: str = input(f"Enter {D} consecutive bits.\n")
+    bits_vec: np.ndarray = np.array(list(bits))
+    bits_vec.reshape(-1, 2)
+
+    # ortho = null_space(bits_vec)
+
+    print(bits_vec.size)
+    # print(ortho, type(ortho))
+
+
+create_image()
